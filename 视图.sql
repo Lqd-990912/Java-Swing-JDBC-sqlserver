95ec15ebc2581438fdb5ca07730b9eaa2ee8f247
东瓜 <39723416+liqingdong-candpython@users.noreply.github.com>
2019-06-20 16:02:18 +0800

    Update 视图.sql

--- 视图.sql
+++ 视图.sql
@@ -1,11 +1,10 @@
-/* 创建视图
+/* 创建视图 */
 --创建计算机学院所有学生的视图
 CREATE VIEW CS_VIEW
 AS
 SELECT SNo '学号',SName '姓名',Sex '性别',Age '年龄',Class '班级'
 FROM Student
 WHERE Dept = '计算机学院';
-
 
 --WITH CHECK OPTION的用法。建立所有计算机学院学生的视图，并要求进行修改和插入操作时仍须保证该视图只有计算机学院的学生
 CREATE VIEW CS_VIEW_V1
@@ -14,7 +13,7 @@ SELECT SNo '学号',SName '姓名',Sex '性别',Age '年龄',Class '班级'
 FROM Student
 WHERE Dept = '计算机学院'
 WITH CHECK OPTION;
-*/
+
 
 /* 查询视图 */
 --建立视图后，就可以把它作为基本表来进行查询操作。
@@ -22,7 +21,6 @@ SELECT *
 FROM CS_VIEW_V1;
 
 
-/* 删除视图 
+/* 删除视图 */
 --删除一个视图 (建立在该视图之上的视图虽然存在，将会失效)
 DROP VIEW CS_VIEW;
-*/
